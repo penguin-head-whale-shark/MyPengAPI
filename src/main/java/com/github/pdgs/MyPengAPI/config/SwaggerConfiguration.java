784dfd2d98988a435dd4f6df72c1f98430e6d667
iqpizza6349 <iqpizza6349@gmail.com>
2021-12-28 09:10:34 +0900

    add documents for api

--- src/main/java/com/github/pdgs/MyPengAPI/config/SwaggerConfiguration.java
+++ src/main/java/com/github/pdgs/MyPengAPI/config/SwaggerConfiguration.java
@@ -8,8 +8,10 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfiguration {
 
     @Bean
@@ -33,5 +35,4 @@ public class SwaggerConfiguration {
                 .version("1")
                 .build();
     }
-
 }
--- src/main/java/com/github/pdgs/MyPengAPI/config/security/SecurityConfiguration.java
+++ src/main/java/com/github/pdgs/MyPengAPI/config/security/SecurityConfiguration.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
